a1b014cf16b016aa922144b081070153cb62a030
Mateo Vargas <mateovargas5@gmail.com>
2026-01-24 14:38:56 -0800

    fixing some export syntax

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,23 +1,17 @@
 import axios from 'axios';
 import { setAuth } from '../reducers/authSlice';
 
-const fetchUser = () => async dispatch => {
+export const fetchUser = () => async dispatch => {
     const res = await axios.get('/api/current_user');
     dispatch(setAuth(res.data || false));
 }
 
-const handleToken = ({ paymentIntentId }) => async (dispatch) => {
+export const handleToken = ({ paymentIntentId }) => async (dispatch) => {
     const res = await axios.post("/api/stripe/fulfill", { paymentIntentId })
     dispatch(setAuth(res.data))
 }
 
-const submitSurvey = (values) => async (dispatch) => {
+export const submitSurvey = (values) => async (dispatch) => {
     const res = await axios.post('/api/surveys', values);
     dispatch(setAuth(res.data));
-}
-
-export {
-    fetchUser,
-    handleToken,
-    submitSurvey
 }
